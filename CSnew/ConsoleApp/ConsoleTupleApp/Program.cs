03c255d4e229774eb21a2c70b238dd4bdf557804
vincent tene <vincent.tene@gmail.com>
2020-11-11 16:34:06 +0100

    new items added

---
+++ CSnew/ConsoleApp/ConsoleTupleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleTupleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var employee = new Employee();
+            employee.Note = ("Vincent",1);
+        }
+    }
+
+    class Employee
+    {
+
+        public (string, double) Note { get; set; }
+
+    }
+}
