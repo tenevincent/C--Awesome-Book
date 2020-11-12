a68601207091f7cb6d934f179941e6d26c4d5024
vincent tene <vincent.tene@gmail.com>
2020-11-12 17:19:38 +0100

    new items added

---
+++ Defensive_Coding_CS_PL/04/demos/APM/ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime? effectiveDate = null;
+
+            Nullable<DateTime> datetime = null;
+
+            Console.WriteLine(datetime.HasValue);
+
+
+            ValidateEffectiveDate(effectiveDate);
+
+
+
+        }
+
+
+        public static bool ValidateEffectiveDate(DateTime? effectiveDate)
+        {
+            if (!effectiveDate.HasValue) return false;
+
+            if (effectiveDate.Value < DateTime.Now.AddDays(7)) return false;
+
+            return true;
+        }
+
+
+    }
+}
