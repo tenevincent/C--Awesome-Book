9e0a2499d18bf37cb319b7c6022278f46be280b4
vincent tene <vincent.tene@gmail.com>
2021-08-12 16:48:45 +0200

    new items added

---
+++ CS-Nutshell/Chapter2/Chapter3.Ab1.Classes/Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleDump;
+using System;
+
+
+
+
+// To call the deconstructor, we use the following special syntax:
+Rectangle rect = new(3, 4);
+(var width, var height) = rect;          // Deconstruction
+Console.WriteLine(width + " " + height);    // 3 4
+
+// We can also use implicit typing:  
+var (x, y) = rect;          // Deconstruction
+Console.WriteLine(x + " " + y);
+
+// If the variables already exist, we can do a *deconstructing assignment*:
+(x, y) = rect;
+Console.WriteLine(x + " " + y);
+
+class Rectangle
+{
+    public readonly float Width, Height;
+
+    public Rectangle(float width, float height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    ~Rectangle()
+    {
+
+    }
+
+ 
+
+    public void Deconstruct(out float width, out float height)
+    {
+        width = Width;
+        height = Height;
+    }
+}
---
+++ CS-Nutshell/Chapter2/Chapter4.Ab1.Delegates/Program.cs
@@ -0,0 +1,41 @@
+﻿using ConsoleDump;
+using System;
+
+
+Transformer t = Square;          // Create delegate instance
+t += Rectangle;
+t += Rectangle;
+
+t(2).Dump();
+
+//int result = t(3);               // Invoke delegate
+//Console.WriteLine(result);      // 9
+
+DoShowDelegate();
+
+int Square(int x)  { Console.WriteLine("Square"); return x* x; }
+int Rectangle(int x) { Console.WriteLine("Rectangle"); return x * x; }
+
+
+void SomeMethod1() => "SomeMethod1".Dump();
+void SomeMethod2() => "SomeMethod2".Dump();
+
+void DoShowDelegate()
+{
+    SomeDelegate d = SomeMethod1;
+    d += SomeMethod2;
+
+    d();
+    " -- SomeMethod1 and SomeMethod2 both fired\r\n".Dump();
+
+    d -= SomeMethod1;
+    d();
+    " -- Only SomeMethod2 fired".Dump();
+}
+
+delegate int Transformer(int x);   // Our delegate type declaration
+
+
+
+
+delegate void SomeDelegate();
